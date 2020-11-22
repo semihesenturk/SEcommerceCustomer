e2c08c61eeaabd70edad2a9d911b3123bc99f330
Semih Esentürk <semihesenturk@outlook.com>
2020-11-22 19:32:22 +0300

    Business layer has started to development

---
+++ SEcommerce.Customer.Business/Concrete/CustomerManager.cs
@@ -0,0 +1,20 @@
+﻿using SEcommerce.Customer.Business.Abstract;
+using SEcommerce.Customer.DataAccess.Abstract;
+using System.Collections.Generic;
+
+namespace SEcommerce.Customer.Business.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        private ICustomerDal _customerDal;
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+        }
+
+        public List<Entites.Concrete.Customer> GetAll()
+        {
+            return _customerDal.GetAll();
+        }
+    }
+}
